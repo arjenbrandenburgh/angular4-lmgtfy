788c2de96e6033b9fac0da3ace2375ce809d0ec7
Arjen Brandenburgh <arjen.brandenburgh@gmail.com>
2017-10-15 14:40:28 +0200

    Restructured the animation to be more reliable on screensizes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,14 @@
 import { Component, ViewChild, OnInit, ElementRef, Renderer, AfterViewInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs/Rx';
-import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
+import { animate, AnimationBuilder, AnimationPlayer, style, trigger, transition } from '@angular/animations';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   animations: [
-    trigger('moveToInput', [
-      transition('bottomRight => atInput', animate('4000ms ease-in', keyframes([
-        style({opacity: 1, left: 'calc(100% - 100px)', top: 'calc(100% - 100px)', offset: 0}),
-        style({opacity: 1, left: 'calc(50% - 190px)', top: '275px',  offset: 0.8}),
-        style({opacity: 0, left: 'calc(50% - 190px)', top: '275px', offset: 1.0})
-      ]))),
-    ]),
-    trigger('moveToButton', [
-      transition('onInput => atButton', animate('2500ms ease-in', keyframes([
-        style({opacity: 1, left: 'calc(50% - 190px)', top: '275px', offset: 0}),
-        style({opacity: 1, left: 'calc(50% - 90px)', top: '315px',  offset: 0.8}),
-        style({opacity: 0, left: 'calc(50% - 90px)', top: '315px', offset: 1.0})
-      ]))),
-    ]),
     trigger('stepSlideIn', [
       transition(':enter', [
         style({transform: 'translateX(-100%)'}),
@@ -30,13 +17,15 @@ import { trigger, state, style, transition, animate, keyframes } from '@angular/
     ])
   ],
 })
-export class AppComponent implements OnInit, AfterViewInit {
+export class AppComponent implements OnInit {
   @ViewChild('inputBox') inputBox: ElementRef;
-  
-  state: string = 'bottomRight';
-  state2: string = 'onInput';
+  @ViewChild('fakePointer') fakePointer: ElementRef;
+  @ViewChild('submitButton') submitButton: ElementRef;
+
+  animationPlayer: AnimationPlayer;
 
   steps: any[] = [];
+  step: number = 1;
 
   stringToType: string = "";
   stringCount: number = 0;
@@ -45,6 +34,7 @@ export class AppComponent implements OnInit, AfterViewInit {
   sub: any;
 
   constructor(private _route: ActivatedRoute,
+              private _animBuilder: AnimationBuilder,
               private _renderer: Renderer) {
   }
 
@@ -54,10 +44,8 @@ export class AppComponent implements OnInit, AfterViewInit {
     this._route.queryParams.subscribe(params => {
         this.stringToType = params['q'];
     });
-  }
 
-  ngAfterViewInit() {
-    this.state = 'atInput';
+    this.animationOrchestrator(); // Start animations
     this.steps.push('Visit Google.com and click the input field');
   }
 
@@ -68,29 +56,59 @@ export class AppComponent implements OnInit, AfterViewInit {
 
     // We've now entered the entire string, and can unsubcribe and start the next animation
     if (this.stringCount >= this.stringToType.length) {
-      this.state2 = 'atButton';
+      this.animationOrchestrator();
       this.steps.push(`Click the button`);
       this.sub.unsubscribe();
     }
   }
 
-  onMoveDone(event) {
-    if (event.fromState !== 'void') {
-      this.sub = Observable.interval(100).takeWhile(() => true).subscribe(() => this.addCharacterToInput());
-      this._renderer.invokeElementMethod(this.inputBox.nativeElement, 'focus');
-      this.steps.push(`Type your question`);
-    }
+  movePointer(x, y, speed, opacity): void {
+    const moveBallAnimation = this._animBuilder.build([
+      animate(`${speed}ms ease`, style({
+          'top': `${y}px`,
+          'left': `${x}px`,
+          'opacity': opacity
+      }))
+    ]);
+
+    this.animationPlayer = moveBallAnimation.create(this.fakePointer.nativeElement);
+    this.animationPlayer.onDone(() => this.animationOrchestrator() );
+    this.animationPlayer.play();
   }
 
-  onMoveDone2(event) {
-    if (event.fromState !== 'void') {
-      const url = `https://www.google.nl/search?q=${encodeURIComponent(this.stringToType)}`;
+  animationOrchestrator(): void {
+    if (this.step === 1) {
+      const x = this.inputBox.nativeElement.offsetLeft;
+      const y = this.inputBox.nativeElement.offsetTop + (this.inputBox.nativeElement.offsetHeight / 2);
+      this.movePointer(x, y, 3500, 1);
+
+    } else if (this.step === 2) {
+      const x = this.inputBox.nativeElement.offsetLeft;
+      const y = this.inputBox.nativeElement.offsetTop + (this.inputBox.nativeElement.offsetHeight / 2);
+      this.movePointer(x, y, 250, 0);
+
+    } else if (this.step === 3) {
+      this.sub = Observable.interval(100).takeWhile(() => true).subscribe(() => this.addCharacterToInput());
+      this.steps.push(`Type your question`);
+
+    } else if (this.step === 4) {
+      const x = this.submitButton.nativeElement.offsetLeft + (this.submitButton.nativeElement.offsetWidth / 2);
+      const y = this.submitButton.nativeElement.offsetTop + (this.submitButton.nativeElement.offsetHeight / 2);
+      this.movePointer(x, y, 3000, 1);
+
+    } else if (this.step === 5) {
       this.buttonClicked = true;
       this.steps.push(`That's it! Was that so hard?`);
-      
       Observable.timer(500).subscribe(() => this.buttonClicked = false );
-      Observable.timer(2000).subscribe(() => window.location.href = url);
+
+      const url = `https://www.google.nl/search?q=${encodeURIComponent(this.stringToType)}`;
+      // Only redirect in production, since it kinda sucks for development
+      if (environment.production) {
+        Observable.timer(3000).subscribe(() => window.location.href = url);
+      }
     }
+
+    this.step++;
   }
 
 }
